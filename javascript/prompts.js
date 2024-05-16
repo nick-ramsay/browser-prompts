8bf397fb2c42c1d96ad1e2d45a2c01d0f9fe5c8e
Nick Ramsay <NickRamsay1989@gmail.com>
2024-05-16 13:37:25 +1000

    Reverting to old version with JavaScript popopups

--- javascript/prompts.js
+++ javascript/prompts.js
@@ -8,41 +8,60 @@ const renderName = (name) => {
         document.getElementById("update-name-btn").style.display = "inline";
         document.getElementById("delete-name-btn").style.display = "inline"
         document.getElementById("enter-name-btn").style.display = "none"
-        
+
         document.getElementById("your-name").innerHTML = "&#128515; Welcome, " + name + "! &#128515;"
     }
 }
 
 const initialNameCheck = () => {
     document.getElementById("update-name-btn").style.display = "none";
-        document.getElementById("enter-name-btn").style.display = "none"
-        document.getElementById("delete-name-btn").style.display = "none"
+    document.getElementById("enter-name-btn").style.display = "none"
+    document.getElementById("delete-name-btn").style.display = "none"
     let yourName = localStorage.getItem("your_name");
 
     if (yourName === null || yourName === "" || yourName === "null") {
-        localStorage.setItem("your_name",prompt("Please enter your preferred name"));
+        localStorage.setItem("your_name", prompt("Please enter your preferred name"));
         renderName(localStorage.getItem("your_name"));
     } else {
         renderName(yourName)
     }
 
-    
 }
 
 const deleteName = () => {
-   localStorage.removeItem("your_name");
-   renderName(localStorage.getItem("your_name"));
+    localStorage.removeItem("your_name");
+    renderName(localStorage.getItem("your_name"));
+    alert("Your name has been deleted 👍");
 };
 
 const updateName = () => {
     let currentName = localStorage.getItem("your_name");
-    let revisedName = prompt("Edit your name below", currentName);
-    localStorage.setItem("your_name", revisedName);
-    renderName(localStorage.getItem("your_name"));
+    let proceed = confirm("Are you sure you'd like to change your name?");
+    if (proceed) {
+        let revisedName = prompt("Edit your name below", currentName);
+        revisedName != null ? localStorage.setItem("your_name", revisedName):"";
+        renderName(localStorage.getItem("your_name"));
+    }
+}
+
+const showCookies = () => {
+    document.getElementById("test-cookie").innerText = document.cookies;
+};
+
+const testElementContent = () => {
+    //let targetElementString = document.querySelector("body > table > tbody > tr > td").innerHTML;
+    let targetElementString = element.innerHTML;
+    let gbStartsAt = targetElementString.indexOf("&nbsp;GB");
+    let gbValue = Number(targetElementString.slice(0,gbStartsAt));
+
+    return (gbValue < 259.6 ? true:false);
+
 }
 
 document.getElementById("delete-name-btn").addEventListener("click", deleteName);
 document.getElementById("update-name-btn").addEventListener("click", updateName);
 document.getElementById("enter-name-btn").addEventListener("click", initialNameCheck);
+document.getElementById("show-cookies-btn").addEventListener("click", showCookies);
+document.getElementById("testElementContentBtn").addEventListener("click", testElementContent);
 
-window.onload = (event) => { initialNameCheck(); };
+window.onload = (event) => { initialNameCheck(); showCookies(); };
